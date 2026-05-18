b5ec9ba73d1e4beb2b15e5825413a9287bb839ae
CodeMaster013 <mgs008@outlook.com>
2026-05-18 19:00:51 -0400

    feat: add /apollo info and /apollo docs commands
    - /apollo info displays organization info with links to GitHub and docs
    - /apollo docs provides link to Apollo Organization documentation
    - Both commands return styled embeds with timestamps and footer attribution

---
+++ src/plugins/utility/commands/apollo.js
@@ -0,0 +1,72 @@
+// Apollo Organization Command
+// Displays information about The A.P.O.L.L.O Organization and documentation links
+
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+
+export default {
+    name: 'apollo',
+    data: new SlashCommandBuilder()
+        .setName('apollo')
+        .setDescription('Get information about The A.P.O.L.L.O Organization')
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('info')
+                .setDescription('Display information about The A.P.O.L.L.O Organization')
+        )
+        .addSubcommand(subcommand =>
+            subcommand
+                .setName('docs')
+                .setDescription('Get the link to Apollo Organization documentation')
+        ),
+    category: 'utility',
+
+    async execute(interaction) {
+        const subcommand = interaction.options.getSubcommand();
+
+        if (subcommand === 'info') {
+            return handleInfo(interaction);
+        } else if (subcommand === 'docs') {
+            return handleDocs(interaction);
+        }
+    }
+};
+
+async function handleInfo(interaction) {
+    const infoEmbed = new EmbedBuilder()
+        .setColor('#5865F2')
+        .setTitle('The A.P.O.L.L.O Organization')
+        .setDescription('An open-source organization dedicated to building innovative, modular, and scalable Discord bots and tools.')
+        .addFields(
+            {
+                name: 'Purpose',
+                value: 'Providing high-quality, community-driven solutions for Discord server management and automation.',
+                inline: false
+            },
+            {
+                name: 'Links',
+                value: '[GitHub Organization](https://github.com/The-A-P-O-L-L-O-Organization)\n[Documentation](https://the-a-p-o-l-l-o-organization.github.io/Apollo-Org-Docs/)',
+                inline: false
+            }
+        )
+        .setTimestamp()
+        .setFooter({
+            text: `Requested by ${interaction.user.tag}`,
+            iconURL: interaction.user.displayAvatarURL()
+        });
+
+    return interaction.reply({ embeds: [infoEmbed] });
+}
+
+async function handleDocs(interaction) {
+    const docsEmbed = new EmbedBuilder()
+        .setColor('#5865F2')
+        .setTitle('Apollo Organization Documentation')
+        .setDescription('My documentation can be found [here](https://the-a-p-o-l-l-o-organization.github.io/Apollo-Org-Docs/)')
+        .setTimestamp()
+        .setFooter({
+            text: `Requested by ${interaction.user.tag}`,
+            iconURL: interaction.user.displayAvatarURL()
+        });
+
+    return interaction.reply({ embeds: [docsEmbed] });
+}
